5d122b79233bd2256d81601000e7b44827521938
BrianServis <brianservis94@gmail.com>
2021-07-11 06:18:09 -0300

    Correcciones en assembler
    Co-Authored-By: paoservis <paoservis@users.noreply.github.com>

--- src/TP/Tercer_Entrega/Final.asm
+++ src/TP/Tercer_Entrega/Final.asm
@@ -12,27 +12,25 @@ a1		  dd		  ?
 a2		  dd		  ?
 a3		  dd		  ?
 b1		  dd		  ?
-variable1		  db		  LEN_LEXEMA DUP(?),'$'
+variable1		  db		  '$'
 p1		  dd		  ?
 p2		  dd		  ?
 p3		  dd		  ?
 b4		  dd		  ?
 b5		  dd		  ?
 _cte1		  dd		  1054.23
-_cte2		  dd		  5343
+_cte2		  dd		  343
 _cte3		  dd		  99999.99
 _cte4		  dd		  99.
 _cte5		  dd		  .9999
 _cte6		  dd		  22
 _cte7		  dd		  9
 _cte8		  dd		  2
-_cte9		  db		  'ewr','$'
-_cte10		  db		  'ewr','$'
+_cte9		  dd		  21
+_cte10		  dd		  2
 _cte11		  dd		  21
 _cte12		  dd		  2
-_cte13		  dd		  21
-_cte14		  dd		  2
-_cte15		  dd		  30
+_cte13		  dd		  30
 
 aux1 dd ?
 aux2 dd ?
@@ -43,15 +41,15 @@ mov AX,@DATA ;
 mov DS,AX ;
 finit ;
 
-fld ; asignacion_cte1
+fld _cte1;asignacion
 fstp p3
-fld ; asignacion_cte2
-fstp p3
-fld ; asignacion_cte3
+fld _cte2;asignacion
+fstp b4
+fld _cte3;asignacion
 fstp a1
-fld ; asignacion_cte4
+fld _cte4;asignacion
 fstp a2
-fld ; asignacion_cte5
+fld _cte5;asignacion
 fstp a3
 fld _cte7
 fld _cte6
@@ -61,8 +59,6 @@ fld _cte8
 fld aux1
 fadd
 fstp aux1
-DisplayFloat ewr 2
-newLine 1
 mov dx, OFFSET read
 mov ah, 9
 int 21h
@@ -73,7 +69,7 @@ DisplayString variable1
 newLine 1
 fld p1
 fstp aux1
-fld b1
+fld b4
 fstp aux2
 while1:
 fld aux1
@@ -83,17 +79,15 @@ fwait
 sahf
 ffree st(0)
 jbe while1
-fld ; asignacionb1
+fld b1;asignacion
 fstp a2
-DisplayFloat ewr 2
-newLine 1
 fld _cte10
 fld _cte9
 fprem
 fstp aux1
 fld _cte12
 fld _cte11
-fidiv
+fdiv
 fstp aux1
 fld b5
 fstp aux1
@@ -106,7 +100,7 @@ fwait
 sahf
 ffree st(0)
 jne else_if1
-fld ; asignacionb5
+fld b5;asignacion
 fstp b4
 jmp end_if1
 else_if1:
@@ -115,7 +109,7 @@ fld a1
 fadd
 fstp aux1
 end_if1:
-jmp end_while
+jmp end_while1
 end_while1:
 
 mov AX, 4C00h
